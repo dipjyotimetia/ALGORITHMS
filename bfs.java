0dfd4e353f89e56f2ef4f10bd46e229685f9532d
Rahul Shetty <rahul.shetty@ves.ac.in>
2017-03-23 11:50:07 +0530

    BFS updated

--- bfs.java
+++ bfs.java
@@ -1,26 +1,98 @@
-public void bfs(int start)
-{    
-        node n = nodes.get(start); 
-        
-        boolean visited[] = new boolean[101];
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class graph {
+
+    int n,e;
+    HashMap<Integer,ArrayList<Integer>> list = new HashMap();
+    boolean v[];
+    int level[];
+    ArrayDeque<Integer> q = new ArrayDeque();
+    
+    //Constructor takes input
+    graph(){
+       
+       n = i();      //no of nodes 
+       e = n-1;      //no of egdes
+       
+       for(int i=0;i<e;i++)               //creating adjacency list
+       {
+            int a = i();
+            int b = i();
+           
+            if(!list.containsKey(a))
+                list.put(a,new ArrayList());
+           
+            if(!list.containsKey(b))
+                list.put(b,new ArrayList());
+           
+           list.get(a).add(b);
+           list.get(b).add(a);
+       }
+       
+       v = new boolean[n+1];
+       level = new int[n+1];
       
-	   ArrayDeque<node> q = new ArrayDeque(); 
+    }
+    
+    public void bfs(int root)
+    {
         
-        q.addFirst(n);
-        visited[1] = true;
-       
-	   while(!q.isEmpty())
+        add(root);           //Add to queue
+        v[root] = true;      //mark visited
+        while(!q.isEmpty())   //while queue not empty
         {
-            node n1 = q.removeLast();
+            root = remove();       //remove first element from queue
+            ArrayList<Integer> ar = list.get(root);
             
-            for(int i : n1.adj)
-            {
-                if(visited[i] == false)
+            for(int i : ar)          //loop its neighbouring nodes
+                if(!v[i])            //If they are not visited
                 {
-                    q.addFirst(nodes.get(i));
-                    visited[i] = true;
+                    add(i);          //add them to end of queue
+                    v[i] = true;     //and mark them visited
                 }
-            }
         }
-       
+    }
+    
+    
+    public static void main(String[] args)
+    {
+        graph g = new graph();
+        g.bfs(1);
+    }
+    
+    
+   void add(int a)
+   {
+        q.addFirst(a);
+   }
+    
+   int remove()
+   {
+        return  q.removeLast();
+   }
+    
+   void p(int a)
+   {
+        System.out.print(a);
+   }    
+   void p(String s)
+   {
+        System.out.print(s);
+   }
+    
+   static Scanner sc = new Scanner(System.in);
+  
+   static long l(){
+        return sc.nextLong();
+    }
+    static int i(){
+        return sc.nextInt();
+    }
+    static String s(){
+        return sc.nextLine();
+    }
 }
